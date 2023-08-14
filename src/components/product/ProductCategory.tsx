dced20b33cf63140beb380d138bc8542cd652727
mintmin <mintmin0320@gmail.com>
2023-08-14 19:48:09 +0900

    feat: 분류 메뉴 및 클릭 이벤트 추가

--- src/components/product/ProductCategory.tsx
+++ src/components/product/ProductCategory.tsx
@@ -1,23 +1,13 @@
+/*
+  상품 목록 페이지 분류 메뉴
+  클릭 시 background와 border-bottom 변경
+*/
+
+import { useState } from 'react';
 import styled from 'styled-components';
 
-function ProductCategory() {
-  const MenuItem: string[] = ['전체', '센서', '모듈', '조명', 'IC', '모터', '보드', '다이오드', '부저', '스위치', '저항', '캐패시터', '트랜지스터', '기타']
-  return (
-    <ProductCategoryBox>
-      {
-        MenuItem.map((item: string) => {
-          return (
-            <ProductCategoryItem>{item}</ProductCategoryItem>
-          )
-        })
-      }
-    </ProductCategoryBox>
-  );
-};
-
-export default ProductCategory;
-
-const ProductCategoryBox = styled.div`
+// CSS
+const StyledProductCategoryBox = styled.div`
   width: 100%;
   height: 100px;
   display: grid;
@@ -26,10 +16,38 @@ const ProductCategoryBox = styled.div`
   border-radius: 8px;
 `;
 
-const ProductCategoryItem = styled.div`
+const StyledProductCategoryItem = styled.div<{ $isSelected?: boolean }>`
   width: 100%;
-  height: 50px;
+  height: 48px;
   display: flex;
   justify-content: center;
   align-items: center;
+  border-bottom: solid 2px ${({ $isSelected }) => ($isSelected ? '#0064ff' : '#fff')};
+  background-color: ${({ $isSelected }) => ($isSelected ? '#f0f0f0' : '#fff')};
+  cursor: pointer;
 `;
+
+function ProductCategory() {
+  const menuItem: string[] = ['전체', '센서', '모듈', '조명', 'IC', '모터', '보드', '다이오드', '부저', '스위치', '저항', '캐패시터', '트랜지스터', '기타'];
+  const [selectedItem, setSelectedItem] = useState<number | null>(null);
+
+  return (
+    <StyledProductCategoryBox>
+      {
+        menuItem.map((item: string, index: number) => {
+          return (
+            <StyledProductCategoryItem
+              key={item}
+              onClick={() => setSelectedItem(index)}
+              $isSelected={selectedItem === index}
+            >
+              {item}
+            </StyledProductCategoryItem>
+          )
+        })
+      }
+    </StyledProductCategoryBox>
+  );
+};
+
+export default ProductCategory;
