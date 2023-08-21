eefaf3b7da46fb2bd4c10b0e5d30a9b7d12274df
mintmin <mintmin0320@gmail.com>
2023-08-21 23:47:05 +0900

    feat: 게시물 페이지네이션 컴포는트 생성

---
+++ src/components/community/PostPagination.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+// CSS
+const PaginationBox = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+function PostPagination() {
+  return (
+    <PaginationBox>PostPagination</PaginationBox>
+  );
+};
+
+export default PostPagination;
