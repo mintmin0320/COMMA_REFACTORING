2ce3d7f3c2ccbac1291f947151bed43e8a908cc1
mintmin <mintmin0320@gmail.com>
2023-08-31 00:53:35 +0900

    feat: 페이지 탭 추가

--- src/routes/HomeRoute.tsx
+++ src/routes/HomeRoute.tsx
@@ -1,8 +1,18 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from 'react';
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import HomePage from '../pages/home/HomePage';
 
 const HomeRoute = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      document.title = 'COMMA';
+    }
+  }, [location]);
+
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
--- src/routes/NoticeRoute.tsx
+++ src/routes/NoticeRoute.tsx
@@ -1,8 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from 'react';
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import NoticePage from '../pages/notice/NoticePage';
 
 const NoticeRoute = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const pageTitle = location.pathname === '/notice' ? '공지사항 - COMMA' : 'COMMA';
+    document.title = pageTitle;
+  }, [location]);
+
   return (
     <Routes>
       <Route path="/" element={<NoticePage />} />
