2a38ab3ceddc22838209c4e13f51d45215e49a6e
mintmin <mintmin0320@gmail.com>
2023-08-30 17:15:21 +0900

    feat: Roboto, ADLaMDisplay 추가

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -4,6 +4,21 @@ import reset from 'styled-reset'
 const GlobalStyle = createGlobalStyle`
   ${reset}
 
+  /* fonts */
+  @font-face {
+    font-family: 'ADLaMDisplay';
+    src: url('/fonts/ADLaMDisplay-Regular.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Roboto';
+    src: url('/fonts/Roboto-Medium.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
   a{
     text-decoration: none;
   }
