ed5748da7c88e44311f2f4f6f84cc724d37f3338
mintmin <mintmin0320@gmail.com>
2023-09-03 20:48:05 +0900

    refactor: 공통 Container 적용

--- src/pages/admin/AdminUserManagementPage.tsx
+++ src/pages/admin/AdminUserManagementPage.tsx
@@ -1,14 +1,8 @@
-import styled from 'styled-components';
-
 import AdminUserManagement from '../../components/admin/AdminUserManagement';
 import AdminSideBar from '../../components/common/layout/sidebar/AdminSideBar';
 
-// CSS
-const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  display: flex;
-`;
+// styles
+import { Container } from '../../components/styles/AdminPageStyles';
 
 const AdminUserManagementPage = () => {
   return (
