1f56fbceb5afc225e9624db4925c332f93258987
mintmin <mintmin0320@gmail.com>
2023-09-25 02:04:22 +0900

    refactor: URL 상수화(constants 폴더로 분리)

---
+++ src/constants/urls.ts
@@ -0,0 +1,8 @@
+// 회원가입
+export const SIGN_UP_URL = `/users/sign-up`;
+
+// 이메일 인증 코드 요청
+export const EMAIL_AUTH_URL = `/account/email/r`;
+
+// 이메일 인증 코드 확인
+export const VERIFY_EMAIL_AUTH_CODE_URL = `/account/email/verify`;
