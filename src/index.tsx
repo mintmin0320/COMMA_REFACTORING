92abc9f642d1ec7c893e48ee5c50b9c7137b3c2b
mintmin <mintmin0320@gmail.com>
2023-12-02 17:48:08 +0900

    refactor: AxiosInstance 및 인터셉터 요청, 응답 로직 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
 import ReactDOM from 'react-dom/client';
-import axios from 'axios';
 
 import './index.css';
 import App from './App';
 import GlobalStyle from './styles/GlobalStyle';
 
 import reportWebVitals from './reportWebVitals';
-
-const BASE_URL = `${process.env.REACT_APP_SERVER_URL}`;
-axios.defaults.baseURL = BASE_URL;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
