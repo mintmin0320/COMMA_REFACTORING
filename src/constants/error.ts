89f639047e0ae0fbe9453bf793615fc88ccceb1c
mintmin <mintmin0320@gmail.com>
2023-12-03 15:41:24 +0900

    refactor: 에러 코드 상수화

---
+++ src/constants/error.ts
@@ -0,0 +1,14 @@
+export const ERROR_CODE = {
+  /* 400 */
+  BAD_REQUEST: 400,
+  /* 401 */
+  UNAUTHORIZED: 401,
+  /* 403 */
+  FORBIDDEN: 403,
+  /* 404 */
+  NOT_FOUND: 404,
+  /* 405 */
+  CONFLICT: 409,
+  /* 500 */
+  INTERNAL_SERVER_ERROR: 500
+};
