556834d5c083f53ec5df28ba51a576439e7d94fc
mintmin <mintmin0320@gmail.com>
2023-12-21 16:44:09 +0900

    feat: jest 유닛 테스트 기본 설정 추가

---
+++ src/components/auth/loginForm/LoginForm.spec.tsx
@@ -0,0 +1,9 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import LoginForm from './LoginForm';
+
+describe("로그인 테스트", () => {
+  test("로그인이 실패하면 에러메시지가 표시된다.", () => {
+    // render(<LoginForm />);
+  })
+})
