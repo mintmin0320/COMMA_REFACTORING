2461506cfe7163b87803baf71354ef95f64c9b2a
mintmin <mintmin0320@gmail.com>
2023-12-27 15:15:04 +0900

    style: 로그아웃 함수 적용 위치 변경

--- src/components/common/layout/side/profile/Profile.tsx
+++ src/components/common/layout/side/profile/Profile.tsx
@@ -1,8 +1,6 @@
-import { useSignOut } from '../../../../../query-hooks/useAuth';
+import { useSignOut } from '../../../../../query-hooks/auth/useSignOut';
 
-import {
-  RiLogoutBoxRFill
-} from '../../../icons';
+import { RiLogoutBoxRFill } from '../../../icons';
 
 import * as S from './profile.style';
 
@@ -11,20 +9,16 @@ import NavItem from '../nav/NavItem';
 const Profile = () => {
   const { mutate: signOut } = useSignOut();
 
-  const handleSignOut = () => {
-    signOut();
-  };
-
   return (
     <S.Aside>
       <S.ProfileBox>
         <S.ImgFrame>
-          <S.ProfileImg src='../images/mintmin.jpg' alt='' />
+          <S.ProfileImg src='../images/mintmin.jpg' alt='프로필 사진' />
         </S.ImgFrame>
       </S.ProfileBox>
       <S.NavBox>
         <NavItem />
-        <S.Button onClick={handleSignOut} aria-label='로그아웃'>
+        <S.Button onClick={() => signOut()} aria-label='로그아웃'>
           <RiLogoutBoxRFill />
         </S.Button>
       </S.NavBox>
